b7c059a0e69013211d8df5f7fa9aa7375960e65c
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-01-08 18:31:46 +0530

    Add files via upload

---
+++ Day-4/Who Will Pay The Bill.py
@@ -0,0 +1,9 @@
+import random
+list1=["Jayanth","Pavan","Ajay","Anitha","Rutwik"]
+#1st Choice
+choice=random.randint(0,len(list1)-1)
+print(list1[choice])
+
+#2nd Choice
+
+print(random.choice(list1))
