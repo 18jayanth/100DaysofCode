524ab90f29a63ba18643accdc3d35a1249da7206
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-01-13 17:13:28 +0530

    Add files via upload

---
+++ Day-006/The Hurdles Loop Challenge.py
@@ -0,0 +1,21 @@
+#In This Challenge the flag is at a specific position , the robot needs to reach it ,doing the same repeared steps , which can be organised with functions and for loop without rewriting the same code
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def movement():
+    
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+for i in range(0,6):
+    movement()
+
+         
+    
