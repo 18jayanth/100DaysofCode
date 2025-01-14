eed3877eadc476e9de35530bfa576b65fd5598b8
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-01-14 11:58:14 +0530

    Add files via upload

---
+++ Day-014/Rules to solve problems.py
@@ -0,0 +1,13 @@
+'''
+Rule:1 Break  big problem into sub problems
+
+Rule:2 By looking at the sub-problems make a to-d0 list
+start with the easiest
+
+Rule:3 turn the problems into comments
+
+Rule:4 write code->run code->fix code repeat
+
+Rule:5 Go to new task
+
+'''
